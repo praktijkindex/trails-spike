1a71ed8ce7153d576999bb7acb44714fa3bd33f3
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-03-19 20:22:42 +0100

    rails g controller months new create show

---
+++ app/controllers/months_controller.rb
@@ -0,0 +1,10 @@
+class MonthsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'months/new'
+
+  get 'months/create'
+
+  get 'months/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
