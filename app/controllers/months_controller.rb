62a6aee2d62770d32f68692459735bad37d8c1cb
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-03-19 20:34:45 +0100

    moths controller drives the simulation start...

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,10 +1,18 @@
 class MonthsController < ApplicationController
   def new
+    @month = 0
   end
 
   def create
+    month_to_create = params[:month][:id].to_i
+    if month_to_create <= 12
+      redirect_to action: "show", id: month_to_create
+    else
+      redirect_to controller: "years", action: "show"
+    end
   end
 
   def show
+    @month = params[:id].to_i
   end
 end
