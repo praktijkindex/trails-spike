7751c5a5334b2f2cefb764147aceb57e26585d78
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-03-19 20:17:05 +0100

    rails g controller years show

---
+++ app/controllers/years_controller.rb
@@ -0,0 +1,4 @@
+class YearsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'years/show'
+
   get 'months/new'
 
   get 'months/create'
