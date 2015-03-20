bb3faa129a00d5fcaec35a2aacdbab1e91439bc3
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-03-20 16:40:09 +0100

    magically appear me and the colleagues

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ActiveRecord::Base
+  class << self
+    def artm
+      find_or_create_by!(name: "artm")
+    end
+
+    def michael
+      find_or_create_by!(name: "Michael")
+    end
+
+    def leon
+      find_or_create_by!(name: "Leon")
+    end
+  end
 end
