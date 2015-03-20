ed397ed17ce2346fe90d4b1b9bc018d20086158c
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-03-20 20:18:09 +0100

    autoload lib/ code

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Trails
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
